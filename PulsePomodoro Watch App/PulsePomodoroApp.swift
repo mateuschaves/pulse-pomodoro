d51fdc5ecae730a7fb93a1659330c446049e1cb1
Mateus Henrique <mateushenriquechaves1@gmail.com>
2024-03-02 01:50:03 -0300

    Initial Commit

---
+++ PulsePomodoro Watch App/PulsePomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulsePomodoroApp.swift
+//  PulsePomodoro Watch App
+//
+//  Created by Mateus Henrique on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PulsePomodoro_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
